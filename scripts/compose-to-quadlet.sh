0e92313330632495f0853c8e4236134c44388815
aanno <aanno@users.noreply.github.com>
2025-12-16 10:51:15 +0100

    working compose conversion script

--- scripts/compose-to-quadlet.sh
+++ scripts/compose-to-quadlet.sh
@@ -1,13 +1,33 @@
 #!/bin/bash -x
 
+# you need:
+#
+# * go install github.com/mikefarah/yq/v4@latest
+#   from https://github.com/mikefarah/yq
+# * cargo install podlet
+
 set -euo pipefail
 . .env
 
 GIT_ROOT=`git rev-parse --show-toplevel`
-NAME=podman-exporter
+NAME=$(yq '.name' docker-compose.yml)
+TARGET_DIR=~/.config/containers/systemd
 
 pushd $GIT_ROOT
 
-envsubst < $1 | podlet compose --pod - # > quadlets/
+# ensure output directory
+mkdir -p $TARGET_DIR | true
+
+envsubst < $1 \
+  | yq '(.volumes[] | select(has("external")) | .external) = false |
+     (.networks[] | select(has("external")) | .external) = false' \
+  | podlet -u -a --overwrite compose --pod - # > quadlets/
+
+# -p 4.8 podman version 4.8: RHEL 9.4 has podman 4.9 but then --pod could NOT be given
+
+for i in $TARGET_DIR/$NAME-* $TARGET_DIR/$NAME.*; do
+  BASE=$(basename $i)
+  ln -f $i quadlets/$BASE
+done
 
 popd
